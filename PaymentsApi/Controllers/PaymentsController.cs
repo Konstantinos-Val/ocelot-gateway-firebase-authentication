cd00e83ddb59e025238599c8fe32647d931988da
Kostas V <kvalais@infolyseis.com>
2019-12-03 15:48:45 +0200

    init project

---
+++ PaymentsApi/Controllers/PaymentsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        // GET: api/Payments
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "payment1", "payment2" };
+        }
+
+        // GET: api/Payments/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return $"Payment-{id}";
+        }
+
+        // POST: api/Payments
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Payments/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/Payments/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ProductsApi/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET: api/Products
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "product1", "product2" };
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return $"Product-{id}";
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
